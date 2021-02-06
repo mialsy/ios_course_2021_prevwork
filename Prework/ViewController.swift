cb411498fc72e5be14d74670bbec662002cf9cb0
Chuxi Wang <wangchuxi94@gmail.com>
2021-02-05 18:56:21 -0800

    add name to unnamed objects

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billAmountTextFiled: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func calculateTip(_ sender: Any) {
+        
+    }
 }
 
